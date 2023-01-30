9a92c2c2bc6224cb1768cda606b7711a522925d4
AdvancedCube <sciencekill@outlook.com>
2023-01-30 19:11:41 +0800

    blog

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Sciencekill\'s blog';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Welcome To Sciencekill\'s blog\n欢迎来到Sciencekill的博客';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'Sciencekill All rights reserved.';
 
   return {
     name,
